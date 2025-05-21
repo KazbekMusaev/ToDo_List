c95149ada0500f7070f6167270e27c67d55fcd2a
KazbekMusaev <145585208+KazbekMusaev@users.noreply.github.com>
2025-05-21 14:38:15 +0300

    Добавил кор дату и первое сохранение при загрузке из апи

--- ToDo List/Modules/MainViewModule/MainInteractor.swift
+++ ToDo List/Modules/MainViewModule/MainInteractor.swift
@@ -13,6 +13,16 @@ protocol MainInteractorProtocol {
 }
 
 final class MainInteractor: MainInteractorProtocol {
+    private func saveItemsInCoreData(_ items: [ToDoItem]) {
+        DispatchQueue.global().async {
+            items.forEach { item in
+                CoreManager.shared.createData(item: item)
+                print("save")
+                print(FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!)
+            }
+        }
+    }
+    
     func updateItem(_ item: ToDoItem) {
         print("updateItem -> MainInteractor")
         presenter?.reloadItem(item)
@@ -23,8 +33,9 @@ final class MainInteractor: MainInteractorProtocol {
     func fetchData() {
         
         let isLoadFirstData = UserDefaults.standard.bool(forKey: "isLoadFirstData")
-        if isLoadFirstData {
+        if !isLoadFirstData {
             NetworkService.getData { [weak self] result in
+                print("load from api")
                 guard let self else { return }
                 switch result {
                 case .success(let todoData):
@@ -37,17 +48,32 @@ final class MainInteractor: MainInteractorProtocol {
                     }
                     
                     UserDefaults.standard.set(true, forKey: "isLoadFirstData")
+                    self.saveItemsInCoreData(filtred)
                     self.presenter?.didLoadTasks(filtred)
+                    
                 case .failure(let error):
                     UserDefaults.standard.set(false, forKey: "isLoadFirstData")
                     self.presenter?.getError(error)
                 }
             }
+        } else {
+            DispatchQueue.global().async { [weak self] in
+                print("load from core data")
+                var model: [ToDoItem] = []
+                CoreManager.shared.toDoItems.forEach { item in
+                    let toDoItem = ToDoItem(
+                        id: Int(item.id),
+                        todo: item.text ?? "nill",
+                        completed: item.isCompleted,
+                        userId: Int(item.userId),
+                        date: item.date,
+                        label: item.label
+                    )
+                    model.append(toDoItem)
+                }
+                self?.presenter?.didLoadTasks(model)
+            }
         }
     }
     
-    
-    private func saveToCoreData(_ items: [ToDoItem]) {
-        // Core Data save logic здесь
-    }
 }
